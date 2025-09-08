aca7fecd7bbb03965a1353f0b5a11c17d0e21e5b
LucasCieczinski <lucas.peres@sptech.school>
2025-09-08 18:55:50 -0300

    Codigo arduino adicionado

---
+++ Arduino/Codigo_MQ2_porcentagem/Codigo_MQ2_porcentagem.ino
@@ -0,0 +1,32 @@
+const int PINO_SENSOR_MQ2 = A0;
+
+const int VALOR_MINIMO = 100;
+const int VALOR_MAXIMO = 1000;
+
+void setup() {
+  Serial.begin(9600);
+}
+
+void loop() {
+  int valorSensor = analogRead(PINO_SENSOR_MQ2);
+
+  float porcentagem = ((float)(valorSensor - VALOR_MINIMO) / (VALOR_MAXIMO - VALOR_MINIMO)) * 100;
+
+  if(porcentagem < 0) {
+    porcentagem = 0;
+  } else if (porcentagem > 100) {
+    porcentagem = 100;
+  }
+
+  Serial.print("Alta:");
+  Serial.print(90);
+  Serial.print(" ");
+  Serial.print("Concentração:");
+  Serial.print(porcentagem);
+  Serial.print(" ");
+  Serial.print("Baixa");
+  Seria.println(15);
+
+
+  delay(2000);
+}
