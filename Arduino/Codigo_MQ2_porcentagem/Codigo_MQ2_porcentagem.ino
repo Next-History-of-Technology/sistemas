d3440c4b28634ece8b2452fd183213dea8fec2ae
LucasCieczinski <lucas.peres@sptech.school>
2025-09-11 22:35:36 -0300

    Ultima atualização pré sprint1

--- Arduino/Codigo_MQ2_porcentagem/Codigo_MQ2_porcentagem.ino
+++ Arduino/Codigo_MQ2_porcentagem/Codigo_MQ2_porcentagem.ino
@@ -1,11 +1,19 @@
+// Bloco 1
 const int PINO_SENSOR_MQ2 = A0;
 
 const int VALOR_MINIMO = 100;
 const int VALOR_MAXIMO = 1000;
 
+
+
+// Bloco 2
 void setup() {
   Serial.begin(9600);
 }
+
+
+
+// Bloco 3
 
 void loop() {
   int valorSensor = analogRead(PINO_SENSOR_MQ2);
@@ -19,11 +27,11 @@ void loop() {
   }
 
   Serial.print("Alta:");
-  Serial.print(90);
+  Serial.print(80);
   Serial.print(" ");
   Serial.print("Média:");
-  Serial.print(60);
-  Serial.print(" ")
+  Serial.print(50);
+  Serial.print(" ");
   Serial.print("Concentração:");
   Serial.print(porcentagem);
   Serial.print(" ");
